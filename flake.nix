2138cac9988592a093faa8e89aaeb27600f4336d
FevyFevy <fevymarine@gmail.com>
2023-10-19 05:31:27 +0200

    first commit current state of flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    tuxedo-nixos = {
+      url = "github:blitz/tuxedo-nixos";
+      # tcc is tested exclusively against nixpkgs 22.11
+      # bcs of that the nixpkgs version is specified as default in the tuxedo-nixos module
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, tuxedo-nixos }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      lib = nixpkgs.lib;
+    in
+      {
+        nixosConfigurations = {
+          tuxedo = lib.nixosSystem {
+  	    inherit system;
+      	    modules = [
+	      ./configuration.nix
+
+	      home-manager.nixosModules.home-manager {
+	        home-manager.useGlobalPkgs = true;
+	        home-manager.useUserPackages = true;
+	        home-manager.users.fevy = {
+	          imports = [ ./home.nix ];
+	        };
+	      }
+
+	      tuxedo-nixos.nixosModules.default {
+	        hardware.tuxedo-control-center.enable = true;
+	        hardware.tuxedo-control-center.package = tuxedo-nixos.packages.x86_64-linux.default;
+	      }
+	    ];
+	  };
+        };
+      };
+}
---
+++ home.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, ... }:
+let
+  # TODO: according to documentation giving the path where the flake.nix resides should work
+  # but seems to not work
+  rebuild-system = pkgs.writeShellScriptBin "rebuild-system" ''
+    nixos-rebuild switch --flake ${config.home.homeDirectory}/etc --verbose
+  '';
+in
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "fevy";
+  home.homeDirectory = "/home/fevy";
+
+  # Packages that should be installed to the user profile.
+  home.packages = [
+    rebuild-system
+
+    pkgs.keepassxc
+
+    pkgs.acpi
+    pkgs.unzip
+    pkgs.zip
+    pkgs.sqlite
+    pkgs.gdb
+
+    # pkgs.fortune
+  ];
+
+  # configuration only, setting it as login shell has
+  # happen on system level (configuration.nix)
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+    };
+  };
+
+  programs.git = {
+   enable = true;
+   userName = "FevyFevy";
+   userEmail = "fevymarine@gmail.com";
+  };
+
+  programs.ssh = {
+    enable = true;
+  };
+
+  programs.htop = {
+    enable = true;
+    settings = {
+      tree_view = true;
+      hide_userland_threads = true;
+      show_program_path = false;
+      highlight_base_name = true;
+      color_scheme = 6;
+    };
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
