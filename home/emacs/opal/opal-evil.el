e3f2c0ebb2111d671dee11836a78ee50d6314355
emibyte <fevymarine@gmail.com>
2025-06-02 20:45:16 +0200

    some basic cute evil bindings and a leader key, less ctrl pressing feels kinda nice ngl frfr)

--- home/emacs/opal/opal-evil.el
+++ home/emacs/opal/opal-evil.el
@@ -18,6 +18,13 @@
   (evil-want-C-u-scroll t)
   (evil-undo-system 'undo-tree)
   :config
+  (evil-set-leader 'normal (kbd "SPC"))
+  (evil-define-key 'normal 'global (kbd "<leader>ff") #'counsel-find-file)
+  (evil-define-key 'normal 'global (kbd "<leader>bk") #'kill-buffer)
+  (evil-define-key 'normal 'global (kbd "<leader>bi") #'ibuffer)
+  (evil-define-key 'normal 'global (kbd "<leader>bf") #'switch-to-buffer)
+  (evil-define-key 'normal 'global (kbd "<leader>bp") #'switch-to-prev-buffer)
+  (evil-define-key 'normal 'global (kbd "<leader>bn") #'switch-to-next-buffer)
   (evil-mode))
 
 (use-package evil-collection
