e85577d0748641e738c7cdbda51055edfdbe62a1
emibyte <fevymarine@gmail.com>
2025-06-03 22:02:49 +0200

    theme switching

--- home/emacs/opal/opal-evil.el
+++ home/emacs/opal/opal-evil.el
@@ -19,6 +19,7 @@
   (evil-undo-system 'undo-tree)
   :config
   (evil-set-leader 'normal (kbd "SPC"))
+  ;; buffer keybinds
   (evil-define-key 'normal 'global (kbd "<leader>ff") #'counsel-find-file)
   (evil-define-key 'normal 'global (kbd "<leader>bd") #'kill-buffer)
   (evil-define-key 'normal 'global (kbd "<leader>bk") #'kill-current-buffer)
@@ -26,6 +27,8 @@
   (evil-define-key 'normal 'global (kbd "<leader>bf") #'switch-to-buffer)
   (evil-define-key 'normal 'global (kbd "<leader>bp") #'switch-to-prev-buffer)
   (evil-define-key 'normal 'global (kbd "<leader>bn") #'switch-to-next-buffer)
+  ;; theme switching
+  (evil-define-key 'normal 'global (kbd "<leader>ht") #'counsel-load-theme)
   (evil-mode))
 
 (use-package evil-collection
