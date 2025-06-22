baa3b6acb31b6d17b1c4c8a00369b511955fdfa3
emibyte <fevymarine@gmail.com>
2025-06-22 18:51:52 +0200

    trying to get racket mode to work

--- home/emacs/opal/opal-racket.el
+++ home/emacs/opal/opal-racket.el
@@ -6,7 +6,10 @@
 
 ;; TODO: racket-xp-mode, bindings like spc m as prefix for all the racket mode specific bindings pls :3
 ;; TODO: paredit for all lisps
-(use-package racket-mode)
+;; TODO: theres still some issues where theres no syntax highlighting if the first buffer i open in emacs is a racket buffer
+(use-package racket-mode
+  :hook (racket-mode . lsp-deferred)
+  :hook (racket-mode . racket-xp-mode))
 
 (provide 'opal-racket)
 ;;; opal-racket.el ends here
