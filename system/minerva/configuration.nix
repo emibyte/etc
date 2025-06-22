8b16f269cd69f62ba7b2d296be4792b9bbdecadc
emibyte <fevymarine@gmail.com>
2025-06-22 18:52:01 +0200

    steam extra proton package

--- system/minerva/configuration.nix
+++ system/minerva/configuration.nix
@@ -138,6 +138,9 @@
     remotePlay.openFirewall = true; # Open ports in firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
   hardware.xone.enable = true;
 
