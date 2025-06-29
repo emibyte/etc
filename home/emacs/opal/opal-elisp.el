e4426af91868dfd06ff010c95cc9b4c4a42a242b
emibyte <fevymarine@gmail.com>
2025-06-29 18:54:12 +0200

    elisp cfg restructure

--- home/emacs/opal/opal-elisp.el
+++ home/emacs/opal/opal-elisp.el
@@ -4,18 +4,17 @@
 
 ;; TODO: setup some useful bindings like eval and stuff via HYDRA
 
-;; this is probably not how u are supposed to do this, but at least it works for now x.x 
-;; also only for elisp for now bcs the lexical binding is kinda mandatory so a file template is needed as opposed to other languages
 (defun opal/insert-elisp-default-template ()
-  "Automatically insert elisp template if buffer new, empty and not a special buffer."
+  "Insert elisp template if buffer new, empty and not a special buffer."
   (yas-minor-mode)
   (when (and (eq (point-min) (point-max))
              (buffer-file-name)
              (not (buffer-modified-p))
-             (not (member (substring (buffer-name) 0 1) '("*" " ")))
-    (yas-insert-snippet "__elisp-basic-template"))))
+             (not (member (substring (buffer-name) 0 1) '("*" " "))))
+    (yas-insert-snippet "__elisp-basic-template")))
 
 (defun opal/elisp-setup ()
+  "Setup for elisp-mode."
   (outline-minor-mode)
   (opal/insert-elisp-default-template))
 
