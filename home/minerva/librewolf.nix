5551079ed72033f886991cae0eb21569be0b8e0b
emibyte <fevymarine@gmail.com>
2025-07-27 19:59:21 +0200

    librewolf

--- home/minerva/librewolf.nix
+++ home/minerva/librewolf.nix
@@ -2,5 +2,30 @@
   programs.librewolf = {
     enable = true;
     package = pkgs.librewolf-bin;
+
+    profiles = {
+      emi = {
+        id = 0;
+        isDefault = true;
+        search = {
+          default = "ddg";
+          force = true;
+          privateDefault = "ddg";
+        };
+        extensions.force = true;
+        settings = {
+          "browser.aboutConfig.showWarning" = false;
+          "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+          "browser.newtabpage.activity-stream.feeds.snippets" = false;
+          "browser.newtabpage.activity-stream.feeds.topsites" = false;
+          "browser.newtabpage.activity-stream.showSearch" = false;
+        };
+      };
+      default = {
+        id = 1;
+        extensions.force = true;
+      };
+    };
   };
 }
