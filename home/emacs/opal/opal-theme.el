b9a12a538234f2cf675e6df270f701d12eeb2b6d
emibyte <fevymarine@gmail.com>
2025-07-27 20:29:19 +0200

    default light theme emacs

--- home/emacs/opal/opal-theme.el
+++ home/emacs/opal/opal-theme.el
@@ -19,8 +19,7 @@
 (use-package ef-themes
   :config
   ;; (load-theme 'ef-maris-dark t nil))
-  (load-theme 'ef-dream t nil))
-  ;; (load-theme 'ef-summer t nil))
+  (load-theme 'ef-summer t nil))
   ;; (load-theme 'kaolin-valley-light t nil))
 
 (use-package doom-modeline
