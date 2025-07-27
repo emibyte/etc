e30fa6fcfd438da199b30bb88ed678f7b40bff05
emibyte <fevymarine@gmail.com>
2025-07-27 20:29:11 +0200

    latte catppuccin

--- home/common/theme.nix
+++ home/common/theme.nix
@@ -1,18 +1,18 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     (catppuccin-kde.override {
-      flavour = ["macchiato"];
+      flavour = ["latte"];
       accents = ["pink"];
       winDecStyles = ["classic"];
     })
     (catppuccin-kvantum.override {
-      variant = "macchiato";
+      variant = "latte";
       accent = "pink";
     })
   ];
   catppuccin = {
     enable = true;
-    flavor = "macchiato";
+    flavor = "latte";
   };
 
   qt = {
