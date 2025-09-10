41f61964de0e0552a34213dd3c93231a1d867aa8
emibyte <fevymarine@gmail.com>
2025-09-10 08:59:24 +0200

    floorp setup (kinda works)

---
+++ home/common/floorp.nix
@@ -0,0 +1,35 @@
+{...}: {
+  programs.floorp = {
+    enable = true;
+
+    profiles = {
+      emi = {
+        id = 0;
+        isDefault = true;
+        search = {
+          default = "ddg";
+          force = true;
+          privateDefault = "ddg";
+        };
+        extensions.force = true;
+        settings = {
+          "browser.aboutConfig.showWarning" = false;
+          "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+          "browser.newtabpage.activity-stream.feeds.snippets" = false;
+          "browser.newtabpage.activity-stream.feeds.topsites" = false;
+          "browser.newtabpage.activity-stream.showSearch" = false;
+        };
+      };
+      default = {
+        id = 1;
+        extensions.force = true;
+        search = {
+          default = "ddg";
+          force = true;
+          privateDefault = "ddg";
+        };
+      };
+    };
+  };
+}
