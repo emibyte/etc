526b6bc823424542bb6964d108dd5c756bc25e9f
emibyte <fevymarine@gmail.com>
2025-11-14 18:08:39 +0100

    flycheck/make and eldoc stuff

--- home/emacs/opal/opal-syntax.el
+++ home/emacs/opal/opal-syntax.el
@@ -2,10 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 
+(add-to-list 'load-path "~/src/flycheck-inline")
 (require 'opal-package)
+(require 'dash)
+(require 'flymake)
+(require 'flycheck-inline)
+(require 'quick-peek)
 
 (use-package flycheck
   :custom
+  (flycheck-display-errors-function (lambda (errors) (ignore errors) nil))
   (eldoc-idle-delay 0.0)
   (flycheck-display-errors-delay 0.1)
   (flycheck-emacs-lisp-load-path 'inherit)
@@ -15,7 +21,6 @@
 
 (use-package quick-peek)
 
-(add-to-list 'load-path "~/src/flycheck-inline")
 (use-package flycheck-inline
   :config
   (setq flycheck-inline-display-function
@@ -29,6 +34,35 @@
             (quick-peek-update ov)))
         flycheck-inline-clear-function #'quick-peek-hide)
   (global-flycheck-inline-mode))
+
+(use-package flymake
+  :config
+  (defun flymake-eldoc-function (report-doc &rest _)
+    "Document diagnostics at point.
+Intended for `eldoc-documentation-functions' (which see)."
+    (let ((diags (flymake-diagnostics (point))))
+      (when diags
+        (funcall report-doc
+                 (mapconcat #'flymake-diagnostic-text diags "\n")
+                 :sort 'error)))))
+
+(use-package eldoc
+  :custom
+  (eldoc-idle-delay 0.1)
+  (eldoc-echo-area-prefer-doc-buffer t)
+  :config
+  (defun opal/eldoc-display-quick-peek (docs interactive)
+    "Display DOCS in a `quick-peek' popup."
+    (when docs
+      (--each docs
+        (if (eq (plist-get (cdr it) :sort) 'error)
+            (let ((msg
+                   (with-current-buffer (eldoc--format-doc-buffer (list it))
+                     (buffer-string))))
+              (quick-peek-show (format "%s" msg)))
+          (eldoc-display-in-echo-area (list it) interactive)))))
+  (setq eldoc-documentation-strategy #'eldoc-documentation-compose)
+  (setq eldoc-display-functions '(eldoc-display-in-echo-area eldoc-display-in-buffer)))
 
 (provide 'opal-syntax)
 ;;; opal-syntax.el ends here
