3057230665f49523c6ca3473e9bd462567726439
emibyte <fevymarine@gmail.com>
2025-12-02 21:49:14 +0100

    update to 25.11 from 25.05

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "my system config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -13,7 +13,7 @@
     catppuccin.url = "github:catppuccin/nix";
 
     stylix = {
-      url = "github:nix-community/stylix/release-25.05";
+      url = "github:nix-community/stylix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
