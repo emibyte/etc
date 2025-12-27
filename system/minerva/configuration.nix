a2d25948e53ada2f953ec11f259ddbb44651264f
emibyte <fevymarine@gmail.com>
2025-12-27 13:14:23 +0100

    steam gamescope

--- system/minerva/configuration.nix
+++ system/minerva/configuration.nix
@@ -205,11 +205,15 @@
   # Steam stuff, probably make a steam.nix
   programs.steam = {
     enable = true;
-    protontricks.enable = true;
+    # protontricks.enable = true;
     gamescopeSession.enable = true;
     remotePlay.openFirewall = true; # Open ports in firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    extraPackages = with pkgs; [
+      gamescope
+      gamemode
+    ];
     extraCompatPackages = with pkgs; [
       proton-ge-bin
     ];
