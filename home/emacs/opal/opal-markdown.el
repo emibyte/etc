093a98dbf191e414340a72a5586cdd6863934236
emibyte <fevymarine@gmail.com>
2026-03-16 21:50:57 +0100

    markdown mode

---
+++ home/emacs/opal/opal-markdown.el
@@ -0,0 +1,13 @@
+;;; opal-markdown.el --- C (and eventually C++) -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require 'opal-package)
+
+(use-package markdown-mode
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+         ("C-c C-e" . markdown-do)))
+
+;;; opal-markdown.el ends here
