272cc2405a5542dafe38ed98c7982f632126e0e3
emibyte <fevymarine@gmail.com>
2026-05-30 23:10:06 +0200

    xfce set removed

--- system/tuxedo/awesome.nix
+++ system/tuxedo/awesome.nix
@@ -37,7 +37,7 @@
     thunar = {
       enable = true;
 
-      plugins = with pkgs.xfce; [
+      plugins = with pkgs; [
         thunar-archive-plugin
         thunar-media-tags-plugin
         thunar-volman
