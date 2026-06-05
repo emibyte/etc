bc22f3e95fe48f2853b68515ca1f9f178d71e212
emibyte <fevymarine@gmail.com>
2026-06-05 11:37:38 +0200

    bumping to 26.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "my system config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -13,7 +13,7 @@
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
 
     stylix = {
-      url = "github:nix-community/stylix/release-25.11";
+      url = "github:nix-community/stylix/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
