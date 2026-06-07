6834af896e48e64469483a26325269e291c0d33a
emibyte <fevymarine@gmail.com>
2026-06-07 16:58:27 +0200

    fix problem with sideline-flymake (don't forget to report upstream)

--- home/emacs/opal/opal-syntax.el
+++ home/emacs/opal/opal-syntax.el
@@ -8,9 +8,39 @@
 
 (use-package sideline-flymake
   :hook (flymake-mode . sideline-mode)
-  :custom
-  (sideline-flymake-display-mode 'line) ;; Show errors on the current line
-  (sideline-backends-right '(sideline-flymake)))
+  :init
+  (setq sideline-flymake-display-mode 'line) ;; Show errors on the current line
+  (setq sideline-backends-right '(sideline-flymake))
+  :config
+  ;; NOTE(emi): there's a problem where eglot errors seem to contain nil sometimes so split-string will fail
+  ;;            and it will cause the errors to not display (should probably report but sideline-flymake report
+  ;;            looks kinda dead)
+  (defun sideline-flymake--show-errors (callback &rest _)
+    "Execute CALLBACK to display with sideline."
+    (when flymake-mode
+      (when-let* ((errors (sideline-flymake--get-errors)))
+        (dolist (err errors)
+          (when-let* ((text (flymake-diagnostic-text err))  ; guard nil
+                      (lines (split-string text "\n"))
+                      (lines (butlast lines (- (length lines) sideline-flymake-max-lines)))
+                      (text (mapconcat #'identity lines "\n"))
+                      (type (flymake-diagnostic-type err))
+                      (type (sideline-flymake--get-level type))
+                      (backend (flymake-diagnostic-backend err))
+                      (face (pcase type
+                              (`error   'sideline-flymake-error)
+                              (`warning 'sideline-flymake-warning)
+                              (`note    'sideline-flymake-note)))
+                      (prefix (pcase type
+                                (`error   sideline-flymake-error-prefix)
+                                (`warning sideline-flymake-warning-prefix)
+                                (`note    sideline-flymake-note-prefix)))
+                      (text (concat prefix text)))
+            (when sideline-flymake-show-backend-name
+              (setq text (format "%s (%s)" text backend)))
+            (add-face-text-property 0 (length text) face nil text)
+            (funcall callback (list text)))))))
+  )
 
 (provide 'opal-syntax)
 ;;; opal-syntax.el ends here
