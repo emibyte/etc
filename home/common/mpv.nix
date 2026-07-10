86a889d6e369345cdc441ee170fd645f124f4bc2
emibyte <fevymarine@gmail.com>
2026-07-10 20:44:52 +0200

    mpv

--- home/common/mpv.nix
+++ home/common/mpv.nix
@@ -1,7 +1,23 @@
 {...}: {
-  programs.mpv.enable = true;
-  programs.mpv.config = {
-    fs = "yes";
-    volume-max = 300;
+  programs.mpv = {
+    enable = true;
+    config = {
+      profile = "high-quality";
+      vo = "gpu-next";
+      gpu-api = "vulkan";
+      hwdec = "yes";
+      dither-depth = "auto";
+      target-colorspace-hint = "yes";
+      audio-exclusive = "no";
+      audio-channels = "auto-safe";
+      sub-fix-timing = "yes";
+      # https://mpv.io/manual/master/#options-interpolation
+      # Reduces stuttering in exchange for blurring in motion scenes ; remove the lines in case you don't want the trade-off
+      video-sync = "display-resample";
+      interpolation = "yes";
+
+      fs = "yes";
+      volume-max = 300;
+    };
   };
 }
