4b1ca2db8382ea0a02adcb3176837bc3dca46b14
emibyte <fevymarine@gmail.com>
2026-08-29 12:49:06 +0200

    pulling out nix settings

--- system/minerva/configuration.nix
+++ system/minerva/configuration.nix
@@ -17,34 +17,6 @@
   # NOTE: this entire thing should probably be refactored into multiple files
   #       especially since it shares a lot of parts with my laptop config
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-  nix.settings.auto-optimise-store = true;
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-  nix.extraOptions = ''
-    # keep-outputs = true
-    # keep-derivations = true
-  '';
-
-  nix.settings.substituters = [
-    "https://cache.nixos.org/"
-    "https://nix-community.cachix.org"
-    "https://hyprland.cachix.org"
-  ];
-
-  nix.settings.trusted-substituters = [
-    "https://hyprland.cachix.org"
-  ];
-
-  nix.settings.trusted-public-keys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-  ];
-
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
