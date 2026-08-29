b9cbf1e99d98c656b2dedfb29816082b173bc5de
emibyte <fevymarine@gmail.com>
2026-08-29 12:51:05 +0200

    formatting

--- system/artemis/configuration.nix
+++ system/artemis/configuration.nix
@@ -1,20 +1,22 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.resumeDevice = "/dev/disk/by-label/swap";
-  boot.kernelParams = [ "resume=/dev/disk/by-label/swap" ];
+  boot.kernelParams = ["resume=/dev/disk/by-label/swap"];
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   hardware.enableRedistributableFirmware = true;
@@ -75,7 +77,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.emily = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    extraGroups = ["wheel" "networkmanager"]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       tree
       vim
@@ -91,9 +93,9 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-      vim 
-      wget
-      git
+    vim
+    wget
+    git
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -138,6 +140,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "26.05"; # Did you read the comment?
-
 }
-
