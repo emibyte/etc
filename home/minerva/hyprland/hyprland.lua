07e886319ee0772a6f6959cdb8c75e51dabc0e10
emibyte <fevymarine@gmail.com>
2026-09-19 09:43:00 +0200

    cursor size hyprland 24 -> 32

--- home/minerva/hyprland/hyprland.lua
+++ home/minerva/hyprland/hyprland.lua
@@ -73,8 +73,8 @@ end)
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XCURSOR_SIZE", "32")
+hl.env("HYPRCURSOR_SIZE", "32")
 
 
 -----------------------
